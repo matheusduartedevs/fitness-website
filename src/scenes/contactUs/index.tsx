6fc45c2598a6d9d373e9f1aa8860f7bd2eea8e78
unknown <matheus.duartesilva42@gmail.com>
2023-07-31 09:25:42 -0300

    implementação dos forms

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion"
 import { SelectedPage } from "../../shared/types"
 import { useForm } from "react-hook-form"
-import ContactUsPageGraphic from '@/ContactUsPageGraphic.png'
+import ContactUsPageGraphic from '@/assets/ContactUsPageGraphic.png'
 import HText from "../../shared/Htext"
 
 type Props = {
@@ -9,7 +9,7 @@ type Props = {
 }
 
 const ContactUs = ({ setSelectedPage }: Props) => {
-    const inputStyles = `mt-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`
+    const inputStyles = `mb-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`
 
     const {
         trigger,
@@ -44,52 +44,69 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                         sapien vel rhoncus. Placerat at in enim pellentesque. Nulla
                         adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
                     </p>
-
-                    <div className="mt-10 justify-between gap-8 md:flex">
-                        <motion.div
-                            className="mt-10 basis-3/5 md:mt-0"
-                            initial="hidden"
-                            whileInView="visible"
-                            viewport={{ once: true, amount: 0.5 }}
-                            transition={{ duration: 0.5 }}
-                            variants={{
-                                hidden: { opacity: 0, x: 50 },
-                                visible: { opacity: 1, x: 0 },
-                            }}
-                        >
-                            <form
-                                target="_blank"
-                                onSubmit={onSubmit}
-                            >
-                                <input
-                                    className={inputStyles}
-                                    type="text"
-                                    placeholder="NAME"
-                                />
-
-                                <input
-                                    className={inputStyles}
-                                    type="text"
-                                    placeholder="EMAIL"
-                                />
-
-                                <textarea
-                                    className={inputStyles}
-                                    placeholder="MESSAGE"
-                                    rows={4}
-                                    cols={50}
-                                />
-
-                                <button
-                                    type="submit" 
-                                    className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 duration-500 hover:text-white"
-                                >
-                                    SUBMIT
-                                </button>
-                            </form>
-                        </motion.div>
-                    </div>
                 </motion.div>
+
+                <div className="mt-10 justify-between gap-8 md:flex">
+                    <motion.div
+                        className="mt-10 basis-3/5 md:mt-0"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: 50 },
+                            visible: { opacity: 1, x: 0 },
+                        }}
+                    >
+                        <form
+                            target="_blank"
+                            onSubmit={onSubmit}
+                        >
+                            <input
+                                className={inputStyles}
+                                type="text"
+                                placeholder="NAME"
+                            />
+
+                            <input
+                                className={inputStyles}
+                                type="text"
+                                placeholder="EMAIL"
+                            />
+
+                            <textarea
+                                className={inputStyles}
+                                placeholder="MESSAGE"
+                                rows={4}
+                                cols={50}
+                            />
+
+                            <button
+                                type="submit"
+                                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 duration-500 hover:text-white"
+                            >
+                                SUBMIT
+                            </button>
+                        </form>
+                    </motion.div>
+
+                    <motion.div
+                        className="mt-10 basis-3/5 md:mt-0"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay: 0.2, duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 },
+                        }}
+
+                    >
+                        <div className="md:before:content-evolvetext w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1]">
+                            <img src={ContactUsPageGraphic} alt="contact-us" className="w-full" />
+                        </div>
+                    </motion.div>
+                </div>
             </motion.div>
         </section>
     )
